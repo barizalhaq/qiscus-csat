784ac699b458966b3656c15c640ae284c4a9c64e
Syahid Firdaus <syahidfrd@gmail.com>
2021-03-18 15:26:10 +0700

    chore: set default extras for layouting

--- app/routes.py
+++ app/routes.py
@@ -153,20 +153,9 @@ def csat_form(csat_code):
         else:
             return redirect('https://qiscus.com')
 
-    # set extras for layouting extras
-    ce = json.loads(csat.app.config.extras)
-    extras = {}
-    extras['background'] = ce['background'] if 'background' in ce else ''
-    extras['background_transparancy'] = ce['background_transparancy'] if 'background_transparancy' in ce else 0 # noqa
-    extras['font_color'] = ce['font_color'] if 'font_color' in ce else '#000000' # noqa
-    extras['logo'] = ce['logo'] if 'logo' in ce else ''
-    extras['color'] = ce['color'] if 'color' in ce else '#005791'
-    if 'rating_min_fb' in ce:
-        extras['rating_min_fb'] = ce['rating_min_fb']
-
     return render_template(
         'csat.html',
-        csat=csat, extras=extras)
+        csat=csat, extras=_set_default_extras(csat.app.config.extras))
 
 
 @web.route('/csat', methods=['POST'])
@@ -203,4 +192,21 @@ def csat_submit():
     csat.submitted_at = datetime.datetime.now()
     csat.update()
 
-    return render_template('closing.html', csat=csat, extras=extras)
+    return render_template(
+        'closing.html',
+        csat=csat,
+        extras=_set_default_extras(csat.app.config.extras))
+
+
+def _set_default_extras(extras):
+    ce = json.loads(extras)
+    extras = {}
+    extras['background'] = ce['background'] if 'background' in ce else ''
+    extras['background_transparancy'] = ce['background_transparancy'] if 'background_transparancy' in ce else 0 # noqa
+    extras['font_color'] = ce['font_color'] if 'font_color' in ce else '#000000' # noqa
+    extras['logo'] = ce['logo'] if 'logo' in ce else ''
+    extras['color'] = ce['color'] if 'color' in ce else '#005791'
+    if 'rating_min_fb' in ce:
+        extras['rating_min_fb'] = ce['rating_min_fb']
+
+    return extras
