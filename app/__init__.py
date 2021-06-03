214707e34b138ebd0e4952171634b2311627e2ab
Muhammad Bariz Alhaq <bariz@qiscus.net>
2021-06-03 20:40:39 +0700

    index response

--- app/__init__.py
+++ app/__init__.py
@@ -79,11 +79,11 @@ def create_app():
     # index route
     @app.route("/")
     def index():
-        # return {
-        #     "status": HTTPStatus.OK,
-        #     "message": "qismo csat service up and running!"
-        # }
+        return {
+            "status": HTTPStatus.OK,
+            "message": "qismo csat service up and running!"
+        }
 
-        return render_template("index.html")
+        # return render_template("index.html")
 
     return app
